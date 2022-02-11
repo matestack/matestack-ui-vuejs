64ae62c6412961809a14785dab95ad6c251d4008
Jonas Jabari <jonas@basemate.com>
2022-02-11 16:21:43 +0100

    add dummy db schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_135044) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_135044) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2021_02_04_135044) do
     t.integer "status"
     t.text "some_data"
     t.text "more_data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "dummy_model_id"
     t.index ["dummy_model_id"], name: "index_dummy_child_models_on_dummy_model_id"
   end
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2021_02_04_135044) do
     t.integer "status"
     t.text "some_data"
     t.text "more_data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "test_models", force: :cascade do |t|
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(version: 2021_02_04_135044) do
     t.integer "status"
     t.text "some_data"
     t.text "more_data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "some_boolean_value"
   end
 
