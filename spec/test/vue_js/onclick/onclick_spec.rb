c7dccf215abbd0f3d552743a5d7980a1ed0dbc82
Aaron Contreras <aaronlcaq@gmail.com>
2023-03-27 17:26:50 -0500

    [ISSUE-5] Add spec coverage for onclick component
    Sets up a spec coverage suite for the new multi-event emission
    behavior for the onclick component as well as a slight refactor of
    the existing specs.

--- spec/test/vue_js/onclick/onclick_spec.rb
+++ spec/test/vue_js/onclick/onclick_spec.rb
@@ -1,46 +1,122 @@
 require 'rails_vue_js_spec_helper'
 include VueJsSpecUtils
 
-describe "Onclick Component", type: :feature, js: true do
+describe "Onclick Component", type: :feature, js: true, aggregate_failures: true do
 
-  it "emits event" do
-    class ExamplePage < Matestack::Ui::Page
-      def response
-        onclick emit: 'show_message' do
-          button 'click me'
+  context 'when given single event' do
+    it "emits event" do
+      class ExamplePage < Matestack::Ui::Page
+        def response
+          onclick emit: 'show_message' do
+            button 'click me'
+          end
+          toggle show_on: 'show_message' do
+            plain "some message"
+          end
         end
-        toggle show_on: 'show_message' do
-          plain "some message"
+      end
+  
+      visit "/example"
+      within 'body' do        
+        expect(page).not_to have_content("some message")
+    
+        click_button 'click me'
+
+        expect(page).to have_content("some message")
+      end
+    end
+  
+    context 'when provided data' do
+      it "emits event with provided data" do
+        class ExamplePage < Matestack::Ui::Page
+          def response
+            onclick emit: 'show_message', data: "some static data" do
+              button 'click me'
+            end
+            toggle show_on: 'show_message' do
+              plain "some message"
+              br
+              plain "{{vc.event.data}}"
+            end
+          end
+        end
+    
+        visit "/example"
+        within 'body' do          
+          expect(page).not_to have_content("some message")
+
+          click_button 'click me'
+
+          expect(page).to have_content("some message")
+          expect(page).to have_content("some static data")
         end
       end
     end
-
-    visit "/example"
-    expect(page).not_to have_content("some message")
-
-    click_button 'click me'
-    expect(page).to have_content("some message")
   end
 
-  it "emits event with data" do
-    class ExamplePage < Matestack::Ui::Page
-      def response
-        onclick emit: 'show_message', data: "some static data" do
-          button 'click me'
+  context 'when given multiple comma-separated events' do
+    it "emits all events" do
+      class ExamplePage < Matestack::Ui::Page
+        def response
+          onclick emit: 'show_message, show_alert' do
+            button 'click me'
+          end
+          toggle show_on: 'show_message' do
+            plain "some message"
+          end
+          toggle show_on: 'show_alert' do
+            plain "some alert"
+          end
         end
-        toggle show_on: 'show_message' do
-          plain "some message"
-          br
-          plain "{{vc.event.data}}"
-        end
+      end
+  
+      visit "/example"
+      within 'body' do        
+        expect(page).not_to have_content("some message")
+        expect(page).not_to have_content("some alert")
+  
+        click_button 'click me'
+
+        expect(page).to have_content("some message")
+        expect(page).to have_content("some alert")
       end
     end
 
-    visit "/example"
-    expect(page).not_to have_content("some message")
+    context 'when provided data' do
+      it "emits all events with provided data" do
+        class ExamplePage < Matestack::Ui::Page
+          def response
+            onclick emit: 'show_message, show_alert', data: "some static data" do
+              button 'click me'
+            end
+            toggle show_on: 'show_message' do
+              plain "some message"
+              br
+              plain "{{vc.event.data}}"
+            end
+            toggle show_on: 'show_alert' do
+              plain "some alert"
+              br
+              plain "{{vc.event.data}}"
+            end
+          end
+        end
+    
+        visit "/example"
 
-    click_button 'click me'
-    expect(page).to have_content("some message")
-    expect(page).to have_content("some static data")
+        within 'body' do          
+          expect(page).not_to have_content("some message")
+          expect(page).not_to have_content("some alert")
+  
+          click_button 'click me'
+
+          expect(page).to have_content("some message")
+          expect(page).to have_content("some alert")
+          expect(page).to have_content("some static data").twice
+        end
+      end
+    end
+    
   end
+  
 end
