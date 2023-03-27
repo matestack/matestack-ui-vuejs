853876dfcbd5eee429bdc610c87d855e2f704b32
Aaron Contreras <aaronlcaq@gmail.com>
2023-03-27 17:16:12 -0500

    [ISSUE-5] Add multi-event emission ability to onclick component
    Adds the multi-event emission ability to the onclick component, while
    abstracting this ability into a mixin that can be used by other
    components as well.

--- spec/dummy/app/matestack/demo/vue_js/pages/first_page.rb
+++ spec/dummy/app/matestack/demo/vue_js/pages/first_page.rb
@@ -10,11 +10,17 @@ class Demo::VueJs::Pages::FirstPage < Matestack::Ui::Page
     # you can call components on pages!
     Demo::VueJs::Components::StaticComponent.call(foo: "bar")
 
-    onclick emit: "foo" do
-      button "emit foo"
+    onclick emit: "foo, bar" do
+      button "emit foo and bar"
     end
 
-    async rerender_on: "foo", id: "some-async" do
+    async rerender_on: "foo, bar", id: "some-async" do
+      div do
+        plain DateTime.now
+      end
+    end
+
+    async show_on: "bar", id: "some-other-async" do
       div do
         plain DateTime.now
       end
