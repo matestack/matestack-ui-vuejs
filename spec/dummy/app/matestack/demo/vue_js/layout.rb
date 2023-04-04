6b6d086361c0f1508a269a621a1c05b18e09c997
Aaron Contreras <aaronlcaq@gmail.com>
2023-04-04 16:06:16 -0500

    [ISSUE-7] Demo desired API on the dummy app's layout
    In order to try out and demonstrate the desired behavior, added
    a set of emitting transition components on the dummy app's layout
    that is available for everyone to try out while developing.

--- spec/dummy/app/matestack/demo/vue_js/layout.rb
+++ spec/dummy/app/matestack/demo/vue_js/layout.rb
@@ -10,11 +10,27 @@ class Demo::VueJs::Layout < Matestack::Ui::Layout
       end
 
       nav do
-        transition path: demo_vue_js_first_page_path do
+        transition path: demo_vue_js_first_page_path,
+                   emit: "page_1_clicked" do
           button "First Page"
         end
-        transition path: demo_vue_js_second_page_path do
+        transition path: demo_vue_js_second_page_path,
+                   emit: "page_2_clicked, bonus_page_2_clicked" do
           button "Second Page"
+        end
+      end
+
+      div do
+        toggle show_on: "page_1_clicked" do
+          plain "`page_1_clicked`` emitted from transition component"
+        end
+
+        toggle show_on: "page_2_clicked" do
+          plain "`page_2_clicked`` emitted from transition component"
+        end
+
+        toggle show_on: "bonus_page_2_clicked" do
+          plain "`bonus_page_2_clicked was also emitted from the transition component"
         end
       end
 
